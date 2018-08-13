6dbe97ae86ab8785efd146d87c45088659b48b12
JungTaepyo <jtp9688@naver.com>
2018-08-13 17:02:55 +0900

    2741

---
+++ Question_Code/src/q3_2741/Main.java
@@ -0,0 +1,17 @@
+package q3_2741;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int i = 0;
+		Scanner scan = new Scanner(System.in);
+		i = scan.nextInt();
+		
+		for (int j = 0; j < i; j++) {
+			System.out.println(j+1);
+		}
+	}
+
+}
