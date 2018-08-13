350ce62ed52988a8e572332c437e4e3d105a7f31
JungTaepyo <jtp9688@naver.com>
2018-08-13 16:05:05 +0900

    2558

--- Question_Code/src/q2_2558/Main.java
+++ Question_Code/src/q2_2558/Main.java
@@ -1,23 +1,17 @@
-package q2_10869;
+package q2_2558;
 
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		int a;
-		int b;
+		int a,b = 0;
 		Scanner scan = new Scanner(System.in);
 		
 		while(scan.hasNext()) {
 			a = scan.nextInt();
 			b = scan.nextInt();
 			System.out.println(a+b);
-			System.out.println(a-b);
-			System.out.println(a*b);
-			System.out.println(a/b);
-			System.out.println(a%b);
 		}
 		scan.close();
 	}
-
 }
