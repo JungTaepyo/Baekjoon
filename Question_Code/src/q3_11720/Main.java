cfc2803f3456ad514e6d08ad59019ea9883247a5
JungTaepyo <jtp9688@naver.com>
2018-08-16 17:13:42 +0900

    11721

--- Question_Code/src/q3_11720/Main.java
+++ Question_Code/src/q3_11720/Main.java
@@ -6,12 +6,11 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
-		String number = scan.next();
 		scan.close();
 		
 		int sum = 0;
 		for (int i = 0; i < n; i++) {
-			sum += number.charAt(i)-'0';
+			
 		}
 		System.out.println(sum);
 	}
