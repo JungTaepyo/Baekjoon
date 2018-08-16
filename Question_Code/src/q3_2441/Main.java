f1d946f2dc04aea2801293089659f29f68a58f48
JungTaepyo <jtp9688@naver.com>
2018-08-16 15:28:21 +0900

    2441

--- Question_Code/src/q3_2441/Main.java
+++ Question_Code/src/q3_2441/Main.java
@@ -1,21 +1,20 @@
-package q3_2439;
+package q3_2441;
 
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+		int i,j,k = 0;
 		int n = 0;
 		Scanner scan = new Scanner(System.in);
 		n = scan.nextInt();
-		int i = 0;
-		int j = 0;
-		int k = 0;
-		for (i = 1; i <= n; i++) {
-			for(j = 0; j < n-i; j++) {
-				System.out.print("*");
-			}
-			for(k = 0; k < n-j ; k++) {
+		
+		for(i = 0; i < n; i++) {
+			for (j = 0; j < i ; j++) {
 				System.out.print(" ");
+			}
+			for (k = 0; k < n-i; k++) {
+				System.out.print("*");
 			}
 			System.out.println();
 		}
